04ab2f431ce00a4ef1c78610b32ad019565f92d1
Macspark <57904588+Macspark@users.noreply.github.com>
2020-02-10 20:25:09 +0400

    w

--- js/script-modal.js
+++
@@ -1,8 +0,0 @@
-const modalCatalogWindow = document.querySelector('.catalog');
-const modalCatalogBtn = document.querySelector('.header__catalog-btn');
-const modalActive = 'catalog--active';
-
-modalCatalogBtn.addEventListener('mouseover', function(event) {
-  event.preventDefault()
-  modalCatalogWindow.classList.toggle(modalActive);
-});
